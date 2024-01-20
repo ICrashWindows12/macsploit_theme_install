a9f27b9309a15acaf03256c67aa648910b71e32e
Icrashwin12 <73936448+ICrashWindows12@users.noreply.github.com>
2024-01-20 17:49:38 +1100

    Update 2themeinstall.sh

--- 2themeinstall.sh
+++ 2themeinstall.sh
@@ -1,13 +1,13 @@
 main() {
     cd ~/Downloads/
-    echo -e "Hacker-style theme initialising"
-    echo -e "Hacker-Style theme downloading"
-    echo -e "receiving xferd.."
+    echo -e "TV Static theme initialising"
+    echo -e "TV Static theme Downloading"
+    echo -e "receiving xferd.. (the downloader lol)"
     curl "https://raw.githubusercontent.com/ICrashWindows12/macsploit_theme_install/main/2.js" -o "./theme-tomorrow_night.js"
     echo -e "Downloaded!"
     echo -e "Applying the theme..."
     mv ~/Downloads/theme-tomorrow_night.js /Applications/Macsploit.app/Contents/Resources/CodeViewer_CodeViewer.bundle/Contents/Resources/ace.bundle/theme-tomorrow_night.js
-    echo -e "Successfully installed theme!"
+    echo -e "Successfully installed theme! custom themes by 901million. XD"
 }
 
 main
