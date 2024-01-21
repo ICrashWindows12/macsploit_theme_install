8c1fb3eea856f3e942bd6076c38d129f9aa63bd7
Icrashwin12 <73936448+ICrashWindows12@users.noreply.github.com>
2024-01-21 14:27:41 +1100

    Create macsploit_theme_selection.sh

---
+++ macsploit_theme_selection.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Prompt user to select a theme
+theme=$(osascript -e 'choose from list {"Hacker-Style UI", "TV Static UI", "QuackR"} with title "Select a Theme or run QuackR"')
+
+# Install selected theme based on user choice
+case $theme in
+  "Hacker-Style UI") curl -s "https://raw.githubusercontent.com/ICrashWindows12/macsploit_theme_install/main/1themeinstall.sh" | bash ;;
+  "TV Static UI") curl -s "https://raw.githubusercontent.com/ICrashWindows12/macsploit_theme_install/main/2themeinstall.sh" | bash ;;
+  "QuackR") curl -s "https://raw.githubusercontent.com/ZackDaQuack/macsploit-custom-themes/main/duck.sh" | bash ;;
+  *) echo "Invalid selection"; exit 1 ;;
+esac
+
+# Additional installation steps if needed
+# ...
+
+echo -e "Running Selected Options..."
